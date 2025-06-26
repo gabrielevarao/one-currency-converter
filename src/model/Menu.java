ecee370c40eaebe5385005c1c8d5f371a982e760
gabrielevarao <gvarao93@gmail.com>
2025-06-26 18:13:00 -0300

    Creating Menu screen

---
+++ src/model/Menu.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scanner = new Scanner (System.in);
+
+    public void init(){
+        System.out.println("""
+                
+                *****************************
+                $     CURRENCY CONVERTER    $
+                *****************************
+                """);
+    }
+
+    public String getBaseCurrency(){
+        System.out.println("""
+                -----------------------------
+                       Nova consulta         
+                -----------------------------
+                O valor está em que moeda? Digite o código correspondente. 
+                Por ex.: USD (dólar americano); EUR (euro); BRL (real brasileiro).""");
+        return scanner.nextLine();
+    }
+
+    public String getTargetCurrency(){
+        System.out.println("""
+                
+                O valor deve ser convertido para que moeda? Digite o código correspondente.
+                Por ex.: USD (dólar americano); EUR (euro); BRL (real brasileiro).""");
+        return scanner.nextLine();
+    }
+
+    public Double getAmount(){
+        System.out.println("""
+                
+                Qual a quantia a ser convertida?""");
+        return scanner.nextDouble();
+    }
+
+}
